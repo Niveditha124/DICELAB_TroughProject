52151e3bfd8cc69b27e9c04384a616ecd2a19785
Vennesa Weedmark <51146303+TinKitten@users.noreply.github.com>
2023-03-10 16:16:50 -0500

    Delete knappBagnold.m

--- knappBagnold.m
+++
@@ -1,8 +0,0 @@
-function newfield = knappBagnold(field,par,dt)
-%knappBagnold operator
-%
-% supposes h&U&C are invariants
-
-newfield = field;
-% solve for K (assumes h is invariant)
-newfield.k_m = max(field.k_m-dt*(par.R*par.g*par.vs*field.c_m) , 0);
