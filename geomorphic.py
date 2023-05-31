035f20a86d7708b4dabf83a6cce3021d60aed88f
Vennesa Weedmark <51146303+TinKitten@users.noreply.github.com>
2023-05-31 14:32:21 -0400

    Ze5 nan issue solved; now not updating values
    array values staying at 0 {also not displaying but that's future us problems}

--- geomorphic.py
+++ geomorphic.py
@@ -18,16 +18,14 @@ def geomorphic(field, par, dt):
 
     # solve for CH
     # start with explicit estimate
-    denom = ((par.vs * par.Rp * 0.6 * (((par.alpha * field.k_m) ** 0.5) / (par.g / np.maximum(h, par.h_min)))) ** 0.08) ### BOOKMARK ###
+    denom = ((par.vs * par.Rp * 0.6 * (((par.alpha * field.k_m) ** 0.5) / (par.g / np.maximum(h, par.h_min)))) ** 0.08)
     Ze5 = ((par.alpha * field.k_m) ** 0.5) / 1 # because we have to initialize the array to something that won't shit the bed, so we can reference it's index :)
     for each in denom:
         for index, item in enumerate(each):
             if item == 0.0: # if it was 0 before it remains the same
-                Ze5[0, index] = (par.alpha * field.k_m[0, index]) ** 0.5 / 1
+                Ze5[0][index] = (par.alpha * field.k_m[0][index]) ** 0.5 / 1
             else: # otherwise it changes to whatever the updated par and field values calculate out to
-                Ze5 = ((((par.alpha * field.k_m[0, index]) ** 0.5) / (par.vs * par.Rp * 0.6 * ((par.alpha * field.k_m[0, index]) ** 0.5) / (par.g / np.maximum(h, par.h_min)))) ** 0.08) ** 5
-
-    print("ze5", Ze5)
+                Ze5[0][index] = ((((par.alpha * field.k_m[0][index]) ** 0.5) / (par.vs * par.Rp * 0.6 * ((par.alpha * field.k_m[0][index]) ** 0.5) / (par.g / np.maximum(h, par.h_min)))) ** 0.08) ** 5
     E = np.maximum((par.p * (1.3 * 10 ** -7)) * par.vs * Ze5 / (1 + (1.3 * 10 ** -7) / 0.3 * Ze5), 0)
     D = np.maximum(par.vs * par.r0 * field.c_m, 0)
     CH_new = np.maximum(CH + dt * (E - D), 0)
@@ -38,8 +36,13 @@ def geomorphic(field, par, dt):
 
     # iterate 10 times with Newton Scheme
     for x in range(10):
-        Ze5 = ((((par.alpha * K_new) ** 0.5) / (par.vs * par.Rp * 0.6 * ((par.alpha * K_new) ** 0.5) / (
-                    par.g / np.maximum(h_new, par.h_min)))) ** 0.08) ** 5
+        denom2 = (par.vs * par.Rp * 0.6 * ((par.alpha * K_new) ** 0.5) / (par.g / np.maximum(h_new, par.h_min)) ** 0.08) ** 5
+        for each in denom2:
+            for index, item in enumerate(each):
+                if item == 0.0:
+                    Ze5[0][index] = (par.alpha * K_new[0][index]) ** 0.5 / 1
+                else:
+                    Ze5[0][index] = ((((par.alpha * K_new[0][index]) ** 0.5) / (par.vs * par.Rp * 0.6 * ((par.alpha * K_new[0][index]) ** 0.5) / (par.g / np.maximum(h_new, par.h_min)))) ** 0.08) ** 5
         E = np.maximum((par.p * (1.3 * 10 ** -7)) * par.vs * Ze5 / (1 + (1.3 * 10 ** -7) / 0.3 * Ze5), 0)
         D = np.maximum(par.vs * par.r0 * C_new, 0)
         CH_new = np.maximum(CH + dt * (E - D), 0)
@@ -68,7 +71,7 @@ def geomorphic(field, par, dt):
     # final update
     newfield = field
     newfield.z_b = field.z_b + dzb
-    if (h_new.all() < par.h_min):
+    if h_new.all() < par.h_min:
         newfield.c_m = field.c_m
     else:
         newfield.c_m = np.minimum(np.maximum(CH_new / np.maximum(h_new, par.h_min), 0), 1)
