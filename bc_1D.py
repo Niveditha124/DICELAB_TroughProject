5534d741ad2b0e13f5b28496e50d9fe19ea26d76
Armani Araujo <aaraujo3@my.yorku.ca>
2023-06-05 11:09:50 -0400

    Minor errors fixed

--- bc_1D.py
+++ bc_1D.py
@@ -8,9 +8,12 @@ def bc_1D(flux=None, field=None, par=None):
     # correct fluxes at upstream inflow section
     s = 0.0 * field.Q_up
 
-    bcflux.q_m[0] = field.Q_up
+    bcflux.q_m[0][0] = field.Q_up
     # bcflux.g_m(1) = s;  (apparently no change)
-    bcflux.sig_r[0] = field.H_up * field.U_up ** 2 + 0.5 * par.g * par.R * (field.C_up * field.H_up ** 2)
-    bcflux.mu[0] = field.C_up * field.Q_up
-    bcflux.kh[0] = field.K_up * field.Q_up
+    bcflux.sig_r[0][0] = field.H_up * field.U_up ** 2 + 0.5 * par.g * par.R * (field.C_up * field.H_up ** 2)
+    bcflux.mu[0][0] = field.C_up * field.Q_up
+    bcflux.kh[0][0] = field.K_up * field.Q_up
+
+    # K_up hardcoded in mirror.py
+    
     return bcflux
