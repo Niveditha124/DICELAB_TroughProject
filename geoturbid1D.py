c58e142067ed5c53077ffe1c4387e1ca4c954c5a
Armani Araujo <aaraujo3@my.yorku.ca>
2023-06-30 17:17:48 -0400

    fluxLHLL2 --> fluxLHLL, init1D --> initMonterrey
    fluxLHLL2 changed to fluxLHLL
    Now uses initMonterrey to test the graphs (since you can see more changes with monterrey than with 1D)

--- geoturbid1D.py
+++ geoturbid1D.py
@@ -10,8 +10,11 @@
 import numpy as np
 import os
 from createFluxY import createFluxY
-os.system('clear')
+
+
+
 import init1D
+from initMonterrey import initMonterrey
 import initpar
 import initrun
 from bc_1D import bc_1D
@@ -34,7 +37,8 @@ n = 200
 o = 1
 geostaticflag = 0
 par = initpar
-field = init1D.field(n, par) # input file
+# field = init1D.field(n, par) # input file
+field = initMonterrey(n, par)
 field_0 = field
 field_prev = field
 # disk output and screen display parameters
@@ -52,11 +56,13 @@ firstTimeStep = 1
 # firstTimeStep = 0;
 iter = 1
 
+os.system('cls')
+
 while field.t < t_end:
     
     print('Iteration ', iter, ': ')
-    if iter == 10:
-        break
+    #    if iter == 10:
+        #        break
     
     if np.logical_or((o == 1), (np.logical_and((o == 2), (iter % 2 == 1)))):
         dt = timestep(field, par)
@@ -76,6 +82,7 @@ while field.t < t_end:
     # screen display:
     if np.logical_or((o == 1), (np.logical_and((o == 2), (iter % 2 == 1)))):
         if dispflag == 1:
+            print('im in the insane if')
             fieldplot(field, field_0, field_prev, par, dt)
             #            pause;
     
@@ -95,8 +102,9 @@ while field.t < t_end:
     # half-step relaxation operator:
     if np.logical_and((o == 2), (iter % 2 == 1)):
         field = relax(field, par, 0.5 * dt, geostaticflag)
-    # extend field left and right:
+    # extend field left and right:    
     field_x = mirror(field)
+
     #    field_y = mirror(swapfield(field));
     # computation of in-cell gradients:
     # note: cell slopes are NOT recomputed for the second step of the predictor-corrector
@@ -104,7 +112,12 @@ while field.t < t_end:
         grad_x = gradientVL(field_x, par, o)
         #        grad_y = gradientVL(field_y,par,o);
     # fluxing scheme (LHLL):
-    flux_x = fluxLHLL_2('x', field_x, grad_x, par, dt) # grad_x can be undefined but maybe we don't care?
+    # Original --> flux_x = fluxLHLL_2('x', field_x, grad_x, par, dt) # grad_x can be undefined but maybe we don't care?
+    flux_x = fluxLHLL(field_x, grad_x, par, dt)
+
+    print('\n', flux_x.sig_l[0][0], flux_x.sig_l[0][1])
+
+
     # impose BC at upstream inflow section
     flux_x = bc_1D(flux_x, field_x, par)
 
@@ -112,6 +125,7 @@ while field.t < t_end:
     # 1D default:
     # Original flux_y line of code
     # flux_y = fluxLHLL_2('y', field_x, grad_x, par, dt) # this is because we have to instantiate flux_y
+    
     flux_y = createFluxY(field)
 
     # print("ln 106 ", flux_x.q_m.shape)
@@ -155,4 +169,5 @@ while field.t < t_end:
                 # time update:
                 field.t = field.t + dt
     iter = iter + 1
+
 
