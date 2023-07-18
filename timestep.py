e4cd0b595dcc05d673c2a1de425f8470eb2fcf7a
Armani Araujo <aaraujo3@my.yorku.ca>
2023-07-17 23:30:41 -0400

    Print statements and changes for testing

--- timestep.py
+++ timestep.py
@@ -4,10 +4,24 @@ import sys
 def timestep(field, par):
     # TIMESTEP compute time step according to the Courant condition
     # cel = ( par.g * max( field.z_m - field.z_b , par.h_min ) ).^0.5;
-    cel = (np.multiply(par.R * par.g * field.c_m, np.maximum(field.z_m - field.z_b, par.h_min)) ** 0.5)
-    vel = (field.u ** 2 + field.v ** 2) ** 0.5
+    # cel = (np.multiply(par.R * par.g * field.c_m, np.maximum(field.z_m - field.z_b, par.h_min)) ** 0.5)
+    
+    temp = np.maximum(field.z_m - field.z_b, par.h_min)
+    temp2 = par.R * par.g 
+    
+    cel = np.sqrt(temp * temp2 * field.c_m)
+    
 
-    # print(cel)
+    # print('par.R: ', par.R)
+    # print('par.g: ', par.g)
+
+    # print('field.z_m: ', field.z_m)
+    # print('field.z_b: ', field.z_b)
+    # print('par.h_min: ', par.h_min)
+
+    # vel = (field.u ** 2 + field.v ** 2) ** 0.5
+    vel = np.sqrt(field.u**2 + field.v**2)
+
     # print(vel)
     sm = vel + cel
     speed_max = np.max(sm)
@@ -19,6 +33,8 @@ def timestep(field, par):
     # dt = par.courant * dl / speed_max
     # np.seterr(divide='ignore', invalid='ignore')
     # print(speed_max)
+    # print('par.courant: ', par.courant)
+    # print('dl: ', dl)
     if speed_max == 0:
         dt = 1
     else:
