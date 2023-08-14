28b2b2c8fce6370fd3bfd44d2cd66f4cec24db82
Armani Araujo <aaraujo3@my.yorku.ca>
2023-08-14 19:59:23 -0400

    MInor testing changes

--- geoturbid1D.py
+++ geoturbid1D.py
@@ -149,7 +149,6 @@ while field.t < t_end:
         x3 = field.x[0]
         y3 = field.z_m[0]
 
-        '''
         # Create the plot
         plt.plot(x1, y1, color=(0.7, 0.7, 0.7))
         plt.plot(x2, y2, color='r')
@@ -166,7 +165,7 @@ while field.t < t_end:
         plt.savefig(filename)
         plt.close()  # Close the figure to clear it for the next run
         
-        '''
+        
         
         '''
         fig, ax1 = plt.subplots()
@@ -244,7 +243,7 @@ while field.t < t_end:
         
         '''
 
-        
+        '''
 
         plt.title('Instant and cumul. bed changes')
 
@@ -270,6 +269,42 @@ while field.t < t_end:
         filename = "images/python/iacbchanges/plot" + str(titleCounter) + ".png"
         plt.savefig(filename)
         plt.close()  # Close the figure to clear it for the next run
+        
+        '''
+
+        # Data Output to file
+        filename = 'data/field' + str(titleCounter) + '.txt'
+        f = open(filename, 'w')
+        output = 'field.x: ' + str(field.x) + '\n\n'
+        output += 'field.y: '+ str(field.y) + '\n\n'
+        output += 'field.z_m: ' + str(field.z_m) + '\n\n'
+        output += 'field.c_m: ' + str(field.c_m) + '\n\n'
+        output += 'field.k_m: ' + str(field.k_m) + '\n\n'
+        output += 'field.z_r: ' + str(field.z_r) + '\n\n'
+        output += 'field.z_b:' + str(field.z_b) + '\n\n'
+        output += 'field.u: ' + str(field.u) + '\n\n'
+        output += 'field.v: ' + str(field.v) + '\n\n'
+        output += 'L1: ' + str(field.L1) + '\n'
+        output += 'S1: ' + str(field.S1) + '\n'
+        output += 'p1: ' + str(field.p1) + '\n'
+        output += 'L2: ' + str(field.L2) + '\n'
+        output += 'S2: ' + str(field.S2) + '\n'
+        output += 'p2: ' + str(field.p2) + '\n'
+        output += 'L3: ' + str(field.L3) + '\n'
+        output += 'S3: ' + str(field.S3) + '\n'
+        output += 'p3: ' + str(field.p3) + '\n'
+        output += 'n: ' + str(field.n) + '\n'
+        output += 'U_up: ' + str(field.U_up) + '\n'
+        output += 'H_up: ' + str(field.H_up) + '\n'
+        output += 'C_up: ' + str(field.C_up) + '\n'
+        output += 'Q_up: ' + str(field.Q_up) + '\n'
+        output += 'K_up: ' + str(field.K_up) + '\n'
+        output += 't: ' + str(field.t) + '\n\n'
+        f.write(output)
+
+
+
+        f.close()
 
         titleCounter = titleCounter + 1
         i_output = i_output + 1
@@ -278,8 +313,6 @@ while field.t < t_end:
     # book-keeping
     # TEMPORARY CHANGE LATER
     # field_prev = field
-    field_prev = deep_copy(field, field_prev)
-    
     # half-step relaxation operator:
     if np.logical_and((o == 2), (iter % 2 == 1)):
         field = relax(field, par, 0.5 * dt, geostaticflag)
