7f546e0ab71d8a327d27ccd7b1c0926266b81505
Armani Araujo <aaraujo3@my.yorku.ca>
2023-10-04 12:08:46 -0400

    Added data file deletion functionality

--- deleteImages.py
+++ deleteImages.py
@@ -11,3 +11,12 @@ dir = ['flowprofile', 'iacbchanges', 'kfrprofile', 'ucprofile']
 for directories in dir:
     for f in os.listdir(os.path.join(root_dir, directories)):
         os.remove(os.path.join(os.path.join(root_dir, directories), f))
+
+deleteDataFlag = input('Would you also like to delete data files? (y/n)')
+
+if deleteDataFlag.strip().lower() == 'y':
+    for f in os.listdir('./data'):
+        os.remove('./data/' + str(f))
+    print('All plots and data files deleted')
+
+else: print('ONLY plots deleted')
