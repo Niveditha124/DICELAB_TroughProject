1e8de58814735a48b0138c1138ce6c2d306d7127
Matthew Bodenstein <matthew_bodenstein@hotmail.com>
2023-12-26 16:54:50 -0500

    ARRAY CHAOS CHANGE!!!!! (everything works properly)
    changed hardcode single variables to arrays to add multiple elements if needed for future changes

--- deepCopier.py
+++ deepCopier.py
@@ -18,15 +18,21 @@ def deep_copy(field: init1D.field, newfield: init1D.field):
     newfield.v = copy.deepcopy(field.v)
     
     # Integers should auto deep copy I think
-    newfield.L1 = copy.deepcopy(field.L1)
-    newfield.S1 = copy.deepcopy(field.S1)
-    newfield.p1 = copy.deepcopy(field.p1)
-    newfield.L2 = copy.deepcopy(field.L2)
-    newfield.S2 = copy.deepcopy(field.S2)
-    newfield.p2 = copy.deepcopy(field.p2)
-    newfield.L3 = copy.deepcopy(field.L3)
-    newfield.S3 = copy.deepcopy(field.S3)
-    newfield.p3 = copy.deepcopy(field.p3)
+    # newfield.L1 = copy.deepcopy(field.L1)
+    # newfield.S1 = copy.deepcopy(field.S1)
+    # newfield.p1 = copy.deepcopy(field.p1)
+    # newfield.L2 = copy.deepcopy(field.L2)
+    # newfield.S2 = copy.deepcopy(field.S2)
+    # newfield.p2 = copy.deepcopy(field.p2)
+    # newfield.L3 = copy.deepcopy(field.L3)
+    # newfield.S3 = copy.deepcopy(field.S3)
+    # newfield.p3 = copy.deepcopy(field.p3)
+    
+    newfield.L = copy.deepcopy(field.L)
+    newfield.S = copy.deepcopy(field.S)
+    newfield.p = copy.deepcopy(field.p)
+
+
     newfield.n = copy.deepcopy(field.n)
     newfield.U_up = copy.deepcopy(field.U_up)
     newfield.H_up = copy.deepcopy(field.H_up)
--- init1D.py
+++ init1D.py
@@ -5,21 +5,26 @@ from scipy.interpolate import interp1d
 class field:
     # defines the magnitude of initial bed perturbations for each reach
     # These perturbations refer to variations in the sediment bed's elevation
-    p3 = 0 # (p3) reach 3
-    p2 = 0 # (p2) reach 2
-    p1 = 0 # (p1) reach 1
+    # p3 = 0 # (p3) reach 3
+    # p2 = 0 # (p2) reach 2
+    # p1 = 0 # (p1) reach 1
+    p = [0,0,0] # p values at each reach
    # defines the lengths of the three reaches
-    L3 = 5000
-    L2 = 2500 # L2 = 6000
-    L1 = 15000 # (L1) length of reach 6000 (L1 = 6000)
+    # L3 = 5000
+    # L2 = 2500 # L2 = 6000
+    # L1 = 15000 # (L1) length of reach 6000 (L1 = 6000)
+    L = [15000,2500, 5000] # Array for all lengths of the three reaches
    # (Lx) computes the sum of all reach lenghts
-    Lx = L1 + L2 + L3
+    # Lx = L1 + L2 + L3     # Manual 
+    Lx = sum(L)             # all elements in the array
+
     # defines the slopes of the three reaches
-    S3 = 0
-    S2 = 25 # S2 = 0.006
-    S1 = 30 # (S1) slope of reach 1 (S1 = 0.013)
+    # S3 = 0
+    # S2 = 25 # S2 = 0.006
+    # S1 = 30 # (S1) slope of reach 1 (S1 = 0.013)
+    S = [30,25,0]
     # (x0) defines the starting point of the x-coordinate
-    x0 = - L1 
+    x0 = - L[0] 
     # (n) = number of cells per unit block length
     n = 200 
     # (dx) computes the grid spacing based off the sum of all reach lengths (Lx) and number of cells (n)
@@ -46,8 +51,8 @@ class field:
     z_r[0][-3:] = -1000 # sets specific values in z_r array
     
     # defines reference points for interpolating the sediment bed elevation 
-    xx = np.array([- L1, 0, L2, L2 + L3])
-    zz = np.array([L1 * S1, 0, - L2 * S2, - L2 * S2 - L3 * S3])
+    xx = np.array([- L[0], 0, L[1], L[1] + L[2]])
+    zz = np.array([L[0] * S[0], 0, - L[1] * S[1], - L[1] * S[1] - L[2] * S[2]])
     set_interp = interp1d(xx, zz, kind='linear', fill_value='extrapolate')
     z_b = set_interp(x)
     # sets specific values in z_r array
@@ -55,24 +60,24 @@ class field:
     z_b[0][-3:] = -1000
     z_r[0][-1] = 1000
     # idientifies certain ranges within the x-coordinate for each reach
-    isX1 = np.argwhere(np.logical_and((x[0] > -L1), (x[0] < 0)))
-    isX2 = np.argwhere(np.logical_and((x[0] > 0), (x[0] < L2)))
-    isX3 = np.argwhere(np.logical_and((x[0] > L2), (x[0] < (L2 + L3))))
+    isX1 = np.argwhere(np.logical_and((x[0] > -L[0]), (x[0] < 0)))
+    isX2 = np.argwhere(np.logical_and((x[0] > 0), (x[0] < L[1])))
+    isX3 = np.argwhere(np.logical_and((x[0] > L[1]), (x[0] < (L[1] + L[2]))))
     # random number generator
     randno = np.random.random()
     # pertubs and alters the sediment bed using a random number and parameters specific to each reach
     for i in isX1:
         sX1 = i[0]
         sX1 = sX1 - 1
-        z_b[0, sX1] = z_b[0, sX1] + (randno * p1 - p1 / 2)
+        z_b[0, sX1] = z_b[0, sX1] + (randno * p[0] - p[0] / 2)
     for i in isX2:
         sX2 = i[0]
         sX2 = sX2 - 1
-        z_b[0, sX2] = z_b[0, sX2] + (randno * p2 - p2 / 2)
+        z_b[0, sX2] = z_b[0, sX2] + (randno * p[1] - p[1] / 2)
     for i in isX3:
         sX3 = i[0]
         sX3 = sX3 - 1
-        z_b[0, sX3] = z_b[0, sX3] + (randno * p3 - p3 / 2)
+        z_b[0, sX3] = z_b[0, sX3] + (randno * p[2] - p[2] / 2)
     # Ensures that z_b is >= to z_r
     z_b = np.maximum(z_b, z_r)
     # Ensures that z_b is >= to z_r
@@ -89,15 +94,18 @@ class field:
 
     def __init__(self, n, par):
         # initializes flow field properties for three reaches
-        self.L1 = 6000
-        self.S1 = 30
-        self.p1 = 0
-        self.L2 = 6000
-        self.S2 = 25
-        self.p2 = 0
-        self.L3 = 5000
-        self.S3 = 0
-        self.p3 = 0
+        # self.L1 = 6000
+        # self.S1 = 30
+        # self.p1 = 0
+        # self.L2 = 6000
+        # self.S2 = 25
+        # self.p2 = 0
+        # self.L3 = 5000
+        # self.S3 = 0
+        # self.p3 = 0
+        self.L = [6000, 6000, 5000]
+        self.S = [30, 25, 0]
+        self.p = [0,0,0]
         # defines parameters for each reach and other flow field variables
         self.n = n
         self.par = par
@@ -119,15 +127,19 @@ class field:
 
     def init1D(self, n, par):
         # initializes parameters and variables for 1D simulation
-        S1 = field.S1
-        S2 = field.S2
-        S3 = field.S3
-        L1 = field.L1
-        L2 = field.L2
-        L3 = field.L3
-        pert1 = field.p1
-        pert2 = field.p2
-        pert3 = field.p3
+        # S1 = field.S1
+        # S2 = field.S2
+        # S3 = field.S3
+        # L1 = field.L1
+        # L2 = field.L2
+        # L3 = field.L3
+        # pert1 = field.p1
+        # pert2 = field.p2
+        # pert3 = field.p3
+        S = field.S
+        L = field.L
+        pert = field.p
+        
         # initialise flow domain:
         # x0 = - L1
         # Lx = L1 + L2 + L3
