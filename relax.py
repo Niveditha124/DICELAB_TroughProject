0b8dd5e6b35da7dff4012c95b10a12172d93366b
Niveditha Sunil <niveditha_sunil@outlook.com>
2026-01-13 14:46:23 -0500

    intermittency

--- relax.py
+++ relax.py
@@ -7,12 +7,14 @@ from hemipelagic import hemipelagic
 
 def relax(field = None,par = None,dt = None, geostaticflag = None):
     #RELAX relaxation (source) operator
-    field = friction(field,par,dt)
-    field = entrainment(field,par,dt)
-    field = geomorphic(field,par,dt)
-    field = knappBagnold(field,par,dt)
-    field = dissipation(field,par,dt)
-    field = hemipelagic(field,par,dt)
+    if field.f == 1: 
+        field = friction(field,par,dt)
+        field = entrainment(field,par,dt)
+        ield = geomorphic(field,par,dt)
+        field = knappBagnold(field,par,dt)
+        field = dissipation(field,par,dt)
+    if field.h == 1:
+        field = hemipelagic(field,par,dt)
 
     newfield = field
     return newfield
