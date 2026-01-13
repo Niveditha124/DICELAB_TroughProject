54789a1dd06c8b92ed012ebeb7102cb7b8735267
Niveditha Sunil <niveditha_sunil@outlook.com>
2026-01-13 14:37:41 -0500

    cleaned up

--- friction.py
+++ friction.py
@@ -5,20 +5,19 @@ def friction(field, par, dt):
 
     # (vel) calculates the norm of the velcocity vector at each point in the flow field, where (u) and (v) are the horizontal and vertical components
     vel = (( field.u ** 2 ) + (field.v ** 2)) ** 0.5
-    # rs = max(vel,(par.g*par.h_min)^0.5)
     rs = np.maximum(vel, (par.g * par.h_min) ** 0.5)
-    # rs = (field.u./max(vel,(par.g*par.h_min)^0.5)) OR (field.u ./ vel)
     rs = field.u / rs
-    # vel = (vel>(par.g*par.h_min)^0.5)
     ls = (vel > ((par.g * par.h_min) ** 0.5)).astype(int)
-    # ix = ls .* rs
     ix = ls * rs
+
     # direction vectors (ix) and (iy), repersent the horizontal and vertical components of the flow velocity
     ix = ((vel > ((par.g * par.h_min) ** 0.5)).astype(int)) * (field.u / (np.maximum(vel, (par.g * par.h_min) ** 0.5)))
     iy = ((vel > ((par.g * par.h_min) ** 0.5)).astype(int)) * (field.v / (np.maximum(vel, (par.g * par.h_min) ** 0.5)))
+    
     # (h) calculates current flow depth/thickness 
     # assumes that (h) is invariant
     h = field.z_m - field.z_b
+
     # solve for U and K 
     # start with explicit solution
     vel_new = vel - (dt * par.alpha * (field.k_m / np.maximum(h, par.h_min)))
@@ -28,6 +27,7 @@ def friction(field, par, dt):
     for i in range(10):
         vel_new = vel - (dt * par.alpha * (K_new / np.maximum(h, par.h_min)))
         K_new = np.maximum(0, (field.k_m / (1 - (par.alpha * (dt * (vel_new / np.maximum(h, par.h_min)))))))
+        
     # redecompose velocity into x and y components and update field
     # (special assignment needed to avoid zeroing small velocities)
     returnValue = field
