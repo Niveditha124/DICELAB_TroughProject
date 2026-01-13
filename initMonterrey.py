c8ad01271460d8826d70234ec0fdce2a7fae3243
Niveditha Sunil <niveditha_sunil@outlook.com>
2026-01-13 14:44:02 -0500

    major updates - reading data from file and new field attributes

--- initMonterrey.py
+++ initMonterrey.py
@@ -1,74 +1,64 @@
 import init1D
 import numpy as np
+import pandas as pd
 
 def initMonterrey(n,par):
-    #INITFIELD initialise flow field
-    #
-    # n = number of cells per unit block length
-
-    # initialise flow domain:
-    x0 = 0 # (x0) defines the initial x-coordinate 
-    Lx = 22000 # (Lx) repersents the length of the flow domain (1 m)
-    y0 = 0 # (y0) defines the initial y-coordinate 
-    Ly = 0.5 # (Ly) computes the total width of the flow domain (1 m) 
-    dx = Lx/n # (dx) computes the grid spacing based off the sum of all reach lengths (Lx) and number of cells (n)
-    dy=dx
+  
     
     # creating field object
     field = init1D.field(n, par) # input file
 
     # creating an array of (x) values
-    #x = (x0-0.5*dx):dx:(x0+Lx+0.5*dx)
-    x = np.arange((x0-0.5*dx), (x0+Lx+0.5*dx) + 1, dx) # +1 to match MATLAB results
+    file = pd.read_csv("R29-Region1_profile.csv")
+    x_from_file = file["x-axis (meters)"].values
+    s = len(x_from_file)    
+    y = np.array([1])
+    x= np.ones((len(y), 1)) * x_from_file
+    y = np.transpose(y) * np.ones((1, x.size))
+    z_b_from_file = file["y-axis (meters)"].values
     
-    # creating a (y) array with a single value of (1)
-    y = np.ones((1,1))
-    #  setting field (x) and (y) attributes using the above arrays to define the grid
-    field.x = np.ones((1,1)) * x # field.x = np.ones(lengt(y), 1) * x
-    field.y = y * np.ones((1, len(x)))
-
-    # setting the top of turbid layer to (-1000) for the whole grid:
-    field.z_m = np.ones( field.x.shape ) * -1000 #.001
-    # field.z_m(field.x<0) = 0.75
-
-    # Setting the turbid concentration to (0) for the whole grid:
-    field.c_m = np.ones(field.x.shape)*0 #.0001
-    # field.c_m(field.x<0) = 0.2
-
-    # setting the turbulent kinetic energy to (0) for the whole grid:
-    field.k_m = np.ones(field.x.shape)*0 #.0001
-
-    # setting the rigid channel bottom under sediment bed to (-2000) for the whole grid:
-    field.z_r = np.ones(field.x.shape) * -2000  # default
-     # setting (z_r) to (-1000) for (x) values greater than (21000):
-    field.z_r[field.x > 21000] = -1000 # field.z_r(field.x>21000) = -1000
+    km_from_file = file["km (units)"].values
+    zm_from_file = file["zm (meters)"].values
+    cm_from_file = file["cm (units)"].values
+    v_from_file = file["v (m/s)"].values
+    u_from_file = file["u (m/s)"].values
     
-  
-    # sediment bed level:
-    field.z_b = np.ones(field.x.shape) * -1000  # default
-    # known points for interpolation
-    xx = [-1e99, 0, 6000, 21000, 21001, 1e99]
-    zz = [0, 0, -78,  -123,  -1000, -1000]
-    field.z_b = np.interp(field.x, xx, zz)
-    # setting the rigid rim around the domain (downstream only) to (1000):
-    field.z_r[-1][-1] = 1000
+    field.z_b = np.ones((len(y), 1)) * z_b_from_file
+    field.k_m = np.ones((len(y), 1)) * km_from_file
+    field.z_r = np.ones((x.shape[0], x.shape[1])) * - 9000
+    field.z_m = np.ones((len(y), 1)) * zm_from_file
+    field.c_m = np.ones((len(y), 1)) * cm_from_file
+    field.u = np.ones((len(y), 1))  * u_from_file
+    field.v = np.ones((len(y), 1)) * v_from_file
+    
+    field.x = x
+    field.y = y
+    
+    field.s = s
+    field.nu = np.ones((x.shape[0],x.shape[1]))*0
+    field.nu2d = np.ones((x.shape[0],x.shape[1]))*0
 
-    # z-ordering condition:
-    field.z_b = np.maximum(field.z_b , field.z_r)
-    field.z_m = np.maximum(field.z_m , field.z_b)
 
-    # velocities:
-    field.u = np.zeros(field.x.shape)
-    field.v = np.zeros(field.x.shape)
+    
 
     # upstream inflow section
     field.U_up = 3.5 # flow velocity
-    field.H_up = 20 # turbidity current depth/thickness
+    field.H_up = 20 #20 # turbidity current depth/thickness
     field.C_up = 0.01 # the layer-averaged volume concentration of suspended sediment carried by the turbidity current
     field.Q_up = field.H_up * field.U_up # the volume transport rate of suspended sediment
     field.K_up = par.CfStar / par.alpha * field.U_up ** 2  # assume turbulence is fully developed at inflow
+
+
+
+    
+
+    
+    
     # time:
     # -----
     field.t = 0
+    field.f = 0
+    field.h = 0
+
     
     return field
