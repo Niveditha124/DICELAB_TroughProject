6aacd48055fd1d9194e740cd0a55bf130ba7c498
Niveditha Sunil <niveditha_sunil@outlook.com>
2026-03-27 16:59:49 -0400

    Update weedmark_ext.py

--- weedmark_ext.py
+++ weedmark_ext.py
@@ -6,7 +6,6 @@ def weedmark_ext(field_w): #generic extension function
     dx = field_w[0, 1] - field_w[0, 0]
     extended_field = np.zeros((1, o_w), float)
     extended_field[0, 0] = field_w[0, 0] - dx
-    for p in range(0, o_w-2):
-        extended_field[0, p + 1] = field_w[0, p]
+    extended_field[0, 1:-1] = field_w[0, :]
     extended_field[0, o_w-1] = field_w[0, o_w-3] + dx
     return extended_field
