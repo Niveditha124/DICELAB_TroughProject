baa61d8b8338379ef70bb3f292c6ef5a35a5ae84
Niveditha Sunil <niveditha_sunil@outlook.com>
2026-03-27 16:57:43 -0400

    Update relax.py

--- relax.py
+++ relax.py
@@ -4,17 +4,33 @@ from geomorphic import geomorphic
 from knappBagnold import knappBagnold
 from dissipation import dissipation
 from hemipelagic import hemipelagic
+from pressure import pressure
+from temp import temp
+import initpar
+import numpy as np
+import sys
 
 def relax(field = None,par = None,dt = None, geostaticflag = None):
     #RELAX relaxation (source) operator
     if field.f == 1: 
+        #tfield = temp(field, initpar)
+
+        #tfield = temp(field, pfield)
+
+        pfield = pressure(field)
         field = friction(field,par,dt)
-        field = entrainment(field,par,dt)
-        ield = geomorphic(field,par,dt)
+        field = entrainment(field, pfield, par,dt)
+        field = geomorphic(field,par,dt)
         field = knappBagnold(field,par,dt)
         field = dissipation(field,par,dt)
+        
     if field.h == 1:
         field = hemipelagic(field,par,dt)
 
+
     newfield = field
+    
+    newfield.ls = pfield.p
+
+    
     return newfield
