479626762bb27dd72563b0fff5fcaa20c071930d
Niveditha Sunil <niveditha_sunil@outlook.com>
2026-03-27 16:54:02 -0400

    Update hemipelagic.py

--- hemipelagic.py
+++ hemipelagic.py
@@ -1,9 +1,18 @@
 import numpy as np
 import sys
+from sun import sun
 
 # HEMIPELAGIC operator accounting for sedimentation from hemipelagic sediments
 def hemipelagic(field,par, dt):
-    z_b_new = field.z_b + dt * par.v_hemi
+
+    if field.t == 0:
+        z_b_new = field.z_b + dt * par.v_hemi
+    else: 
+        I = sun(field,par)
+        z_b_new = field.z_b + dt* par.v_hemi - (I)
+
+    
+    
     
     # final update:
     newfield = field
